4e2b7dd5dd3d1408410d1781e4f77c6b461c9374
debaze <joris.derese@gmail.com>
2015-03-02 16:31:32 +0100

    added detailed weather modal

--- client/client.js
+++ client/client.js
@@ -26,7 +26,12 @@ updateMarker= function(){
            marker.setLatLng(coordArray);
            return marker.update();
 }
+Template.detailedForecastModal.helpers({
+          detforecast : function(){
+            return Session.get("detForecast");
+          }
 
+});
 Template.weather.helpers({
         forecast : function() {
         return Session.get("WeatherSituation");
@@ -70,16 +75,34 @@ Template.body.events({
         if (result !== "error"){
           Session.set("WeatherSituation",result.desc + ", "+ Math.floor(result.temp - 273)+ "\xB0C");
           Session.set("coord", [result.lat, result.lon]);
-          updateMarker();
+                  updateMarker();
         } else {
           Session.set("WeatherSituation", "No weather information for this location. Please try again");
           Session.set("locatie", false);
           Session.set("coord", [-1000, -1000]); //Hide marker
-          updateMarker();
+                 updateMarker();
         }
         });
-
       return false;  //Avoid standard browser reload on form submit
+      },
+      'click .detailedForecast': function(event){
+      var _locatie = Session.get("locatie");
+      Session.set("detForecast", [{content:"loading..."}, {content:"loading..."}, {content:"loading..."}])
+        Meteor.call('getDetailedForecast', _locatie, function(err, result){
+          if (result !=="error"){
+            var detailedArr = [];
+            detailedArr[0] = {"content" : result.desc1 + ", "+ Math.floor(result.temp1)+ "\xB0C"};
+            detailedArr[1] = {"content" : result.desc2 + ", "+ Math.floor(result.temp2)+ "\xB0C"};
+            detailedArr[2] = {"content" : result.desc3 + ", "+ Math.floor(result.temp3)+ "\xB0C"};
+            Session.set("detForecast", detailedArr);
+          }else{
+               Session.set("detForecast", false);
+
+          }
+      
+      
+        });
+
       },
 
       'submit .new-shout' : function(event){
