478bca1589116732bde20507678f41c54f7dc267
debaze <joris.derese@gmail.com>
2015-05-05 15:54:50 +0200

    scroll down shouts after page rendered

--- client/templates/shoutbox/shoutbox.js
+++ client/templates/shoutbox/shoutbox.js
@@ -1,3 +1,8 @@
+var scrollDown = function(){
+  var content = document.getElementById('shoutContent');
+  content.scrollTop = content.scrollHeight;
+};
+
 Template.shoutbox.helpers({
   shout: function(){
     var shoutCursor =  Shouts.find({}, {sort: {createdAt: 1}});
@@ -13,11 +18,11 @@ Template.shoutbox.helpers({
 });
 
 Template.shoutbox.rendered = function(){
+  scrollDown();                             //scroll down after shouts are rendered in DOM
   this.find('#shoutContent')._uihooks = {
-    insertElement: function (node, next){       //insert new shout and scroll down
+    insertElement: function (node, next){   //insert new shout and scroll down
       $(node).insertBefore(next);
-      var content = document.getElementById('shoutContent');
-      content.scrollTop = content.scrollHeight;
+      scrollDown();
     }
   }
 };
